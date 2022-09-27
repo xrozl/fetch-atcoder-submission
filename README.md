3bdcd1d32eda60c53868bc6d7d286593c5ecf2f9
yu7400ki <yu7400ki@gmail.com>
2022-09-28 01:57:22 +0900

    コードの保存

--- README.md
+++ README.md
@@ -1,13 +1,34 @@
-# 環境設定
+## このプログラムについて
+このプログラムはAtCoder上にアップロードされたACかつ最新のコードを全て取得するものです。
+取得はAtCoderのユーザーIDを指定して行います。
 
-## 前提
+### 使い方
+```
+git clone https://github.com/yu7400ki/fetch-atcoder-submission.git
+cd fetch-atcoder-submission
+pip install -r requirements.txt
+python ./src/main.py
+
+Please input your atcoder id:{your id}
+```
+もしpoetryがインストールされていれば```pip```の代わりに以下が使えます。
+```
+poetry install --without dev
+```
+
+取得したコードは```./dist```に保存されます。
+
+
+## 開発環境
+
+### 前提
 ```
 python 3.10
 poetry 1.2
 git
 ```
 
-## セットアップ
+### セットアップ
 ```
 git init
 poetry shell
--- src/main.py
+++ src/main.py
@@ -1,7 +1,70 @@
-def main():
-    # TODO
+import asyncio
+import json
+import os
+
+from tqdm import tqdm
+
+from atcoder import fetch_all_submissions, fetch_submission_code
+
+
+def get_extension(language):
+    for v in extensions:
+        if language.startswith(v):
+            return f".{extensions[v]}"
+    return ""
+
+
+def read_json(path):
+    with open(path, mode="r", encoding="UTF-8") as f:
+        return json.load(f)
+
+
+def write_code(submission, code):
+    language = submission["language"]
+    contest_id = submission["contest_id"]
+    problem_id = submission["problem_id"]
+    extension = get_extension(language)
+
+    dir_path = f"./dist/{language}/{contest_id}/"
+    file_path = f"{dir_path}{problem_id}{extension}" if extension != "" else f"{dir_path}{problem_id}"
+
+    os.makedirs(dir_path, exist_ok=True)
+    with open(file_path, mode="w", encoding="UTF-8", newline="") as f:
+        f.write(code)
+
+    return
+
+
+def filter_ac_and_latest(submissions):
+    _submissions = sorted(submissions, key=lambda x: x["epoch_second"], reverse=True)
+    history = set()
+    result = []
+
+    for submission in _submissions:
+        if submission["result"] == "AC":
+            pair = (submission["language"], submission["problem_id"])
+            if pair not in history:
+                history.add(pair)
+                result.append(submission)
+
+    return result
+
+
+async def main():
+    all_submission = fetch_all_submissions(USER_ID)
+    loop = asyncio.get_event_loop()
+    asyncio.set_event_loop(loop)
+
+    for submission in tqdm(filter_ac_and_latest(all_submission)):
+        await asyncio.sleep(1.5)
+        code = fetch_submission_code(submission["contest_id"], submission["id"])
+        loop.run_in_executor(None, write_code, submission, code)
+
+    print("All latest AC codes have been fetched.")
     return
 
 
 if __name__ == "__main__":
-    main()
+    USER_ID = input("Please input your atcoder id:")
+    extensions = read_json("./src/extensions.json")
+    asyncio.run(main())
